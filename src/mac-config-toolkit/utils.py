f717e14fe39f86f85e18c1ddc06f7695ec3cc49d
Cameron Shannon <cshannon@choicepays.com>
2023-04-26 09:56:47 -0700

    Added a FileTools class. Includes a context manager for json files.

--- src/mac-config-toolkit/utils.py
+++ src/mac-config-toolkit/utils.py
@@ -1,4 +1,6 @@
+import json
 import subprocess
+from contextlib import contextmanager
 from pathlib import Path
 
 
@@ -14,6 +16,35 @@ class DirTools:
         dir_names = [d.name for d in dir_list]
 
         return sorted(dir_names)
+
+
+class FileTools:
+    def __init__(self, path):
+        self.path = Path(path)
+
+    def get_file_names(self):
+        # get list of files in given path
+        file_list = [f for f in self.path.iterdir() if f.is_file()]
+
+        # get names of files
+        file_names = [f.name for f in file_list]
+
+        return sorted(file_names)
+
+    @contextmanager
+    def json_file_manager(self, file_name):
+        file = self.path / file_name
+        try:
+            with open(file, "r") as f:
+                data = json.load(f)
+            yield data
+
+        except Exception as e:
+            print(f"Error: {e}")
+            yield None
+        finally:
+            print("File closed...")
+            pass
 
 
 class UseSubprocess:
